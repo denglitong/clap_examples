a65dd35173ba9c647a49287debf93701c9078541
denglitong <denglitong@xiaomi.com>
2019-11-02 16:19:53 +0800

    feat: clap_app!() macro use
    Signed-off-by: denglitong <denglitong@xiaomi.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+#[macro_use]
 extern crate clap;
 
 mod quick_example;
 
 fn main() {
     // quick_example::a::main();
-    quick_example::b::main();
+    // quick_example::b::main();
+    quick_example::c::main();
 }
